879846fd9e60c7da6cedde8f6d7a1759976071c8
GoldenStone02 <thegoldenstone02@gmail.com>
2022-07-18 14:50:00 +0800

    Moved Logic file

--- server/iot_logic.py
+++ server/iot_logic.py
@@ -1,14 +1,14 @@
 # Need to follow structure of ppt 
 
 # Importing various functions for sensor logic via the sensors folder.
-from ..sensors.buzzer import buzzer_on
-from ..sensors.camera import take_pic
-from ..sensors.keypad import keypad
-from ..sensors.LCD import LCD
-from ..sensors.LED import LED_State
-from ..sensors.PIR import PIR
-from ..sensors.rfid import rfid
-from ..sensors.servo import servo
+from sensors.buzzer import buzzer_on
+from sensors.camera import take_pic
+from sensors.keypad import keypad
+from sensors.LCD import LCD
+from sensors.LED import LED_State
+from sensors.PIR import PIR
+from sensors.rfid import rfid
+from sensors.servo import servo
 
 # Main Programme
 while True:
